f7b25ac048dee5de6d52ef47cfe762e05fcf1b1a
NamMinji <73888823+rhdiddl9911@users.noreply.github.com>
2021-02-17 11:32:08 +0900

    프로젝트 파일 생성

---
+++ src/test/java/com/heatokpia/HeatokpiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heatokpia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatokpiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
