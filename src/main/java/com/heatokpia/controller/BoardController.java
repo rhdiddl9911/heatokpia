3c47ebc8c7d8a98ddf313640af88fa5e7a83ab09
NamMinji <73888823+rhdiddl9911@users.noreply.github.com>
2021-02-19 11:42:24 +0900

    board controller 글 입력 실패시 반환 수정

--- src/main/java/com/heatokpia/controller/BoardController.java
+++ src/main/java/com/heatokpia/controller/BoardController.java
@@ -5,11 +5,13 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -45,7 +47,7 @@ public class BoardController {
 	
 	// 익명 사용자 글추가
 	@PostMapping("/{category}/new")
-	public HttpStatus boardWriteDo(
+	public @ResponseBody ResponseEntity<?> boardWriteDo(
 			@PathVariable BoardCategory category,
 			@RequestBody @Valid BoardNonMember data,
 			@ClientIP String ip) {
@@ -53,12 +55,12 @@ public class BoardController {
 		logger.info("boardWriteDo");
 		
 		if(ip == null && category == null) {
-			return HttpStatus.BAD_REQUEST;
+			return new ResponseEntity<>("다시 시도해 주세요", HttpStatus.BAD_REQUEST);
 		}
 		
 		// 데이터 입력 시도
 		service.write(data, ip, category);
-		return HttpStatus.OK;
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
 }
