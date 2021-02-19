724e364b53a84bbb8224d278d1cc813a3e2935ed
NamMinji <73888823+rhdiddl9911@users.noreply.github.com>
2021-02-19 15:40:04 +0900

    boardservice logger추가

--- src/main/java/com/heatokpia/Service/BoardService.java
+++ src/main/java/com/heatokpia/Service/BoardService.java
@@ -1,5 +1,7 @@
 package com.heatokpia.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BoardService {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private final BoardMapper boardMapper;
 	private final PasswordEncoder passEncoder;
 	
@@ -22,5 +25,6 @@ public class BoardService {
 		data.setCategory(category.getCategoryNum());
 		data.setPassword(passEncoder.encode(data.getPassword()));
 		boardMapper.save(data);
+		logger.info("write:"+data.toString());
 	}
 }
