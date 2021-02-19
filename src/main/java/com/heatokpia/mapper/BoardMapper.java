2f399652a64435e8b24ad4f14fc9821a91994e74
NamMinji <73888823+rhdiddl9911@users.noreply.github.com>
2021-02-19 16:46:46 +0900

    board Mapper작성 타이틀,생성날짜를 가진 List 반환되어짐

--- src/main/java/com/heatokpia/mapper/BoardMapper.java
+++ src/main/java/com/heatokpia/mapper/BoardMapper.java
@@ -3,8 +3,8 @@ package com.heatokpia.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
-import com.heatokpia.domain.BoardCategory;
 import com.heatokpia.dto.BoardForListDTO;
 import com.heatokpia.dto.BoardNonMemberDTO;
 
@@ -13,5 +13,7 @@ public interface BoardMapper {
 	// 익명유저 글작성
 	public void save(BoardNonMemberDTO data);
 	
+	// 글 목록 불러오기
+	public List<BoardForListDTO> findAllByCategory(@Param("categorynum") int categorynum);
 	
 }
