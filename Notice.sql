e6ce6d6c93c9eb7d4d13f4e552d9666cb11b0a74
NamMinji <73888823+rhdiddl9911@users.noreply.github.com>
2021-02-26 22:59:46 +0900

    sql hit 삭제

--- Notice.sql
+++ Notice.sql
@@ -1,12 +1,11 @@
 DROP TABLE NOTICE_BOARD CASCADE CONSTRAINTS;
-select * from NOTICE_BOARD;
+
 CREATE TABLE notice_board (
        seq                  INTEGER NOT NULL,
        title                VARCHAR2(300) NOT NULL,
        content              VARCHAR2(3000) NOT NULL,
        createdate           DATE NOT NULL,
        updatedate           DATE NULL,
-       hit                  INTEGER NULL,
        id                   VARCHAR2(20) NOT NULL,
        category	    		VARCHAR2(1) NOT NULL,
        PRIMARY KEY (seq)
@@ -22,3 +21,5 @@ CREATE SEQUENCE seq_notice_board
 INCREMENT BY 1
 MINVALUE 1
 NOCYCLE;
+
+select * from NOTICE_BOARD;
