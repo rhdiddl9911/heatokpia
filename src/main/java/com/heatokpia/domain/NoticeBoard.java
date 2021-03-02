b0643d9124f591fef72b50837f1a0360e12a1674
NamMinji <73888823+rhdiddl9911@users.noreply.github.com>
2021-03-02 10:49:29 +0900

    notice domain에 valid조건, 메세지 추가

--- src/main/java/com/heatokpia/domain/NoticeBoard.java
+++ src/main/java/com/heatokpia/domain/NoticeBoard.java
@@ -1,5 +1,10 @@
 package com.heatokpia.domain;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +15,13 @@ import lombok.NoArgsConstructor;
 public class NoticeBoard {
 
 	private int seq;
+	
+	@Size(max = 100, message = "제목이 너무 깁니다.")
+	@NotBlank(message = "제목을 입력해주세요")
 	private String title;
+	
+	@Size(max = 1000, message = "내용이 너무 깁니다.")
+	@NotBlank(message = "내용을 입력해주세요")
 	private String content;
 	private String createdate;
 	private String updatedate;
